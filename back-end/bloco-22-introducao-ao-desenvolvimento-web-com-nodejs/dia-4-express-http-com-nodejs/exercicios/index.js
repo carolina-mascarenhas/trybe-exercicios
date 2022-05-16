b16b72ea2b0030c274d1ad06e9b751629fe7c791
Carolina Mascarenhas <carolina.mascarenhass96@hotmail.com>
2022-05-16 11:28:35 -0300

    exercicios 1 ao 4 do dia

---
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/exercicios/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get('/ping', (_req, res) => {
+  return res.json({ message: 'pong' });
+});
+
+app.post('/hello', (req, res) => {
+  const { name } = req.body;
+  return res.json({ "message": `Hello, ${name}!` });
+});
+
+app.post('/greetings', (req, res) => {
+  const { name, age } = req.body;
+  if (age > 17) {
+    return res.status(200).json({ "message": `Hello, ${name}!` })
+  } else {
+    return res.status(401).json({ "message": "Unauthorized" })
+  }
+});
+
+app.put('/users/:name/:age', (req, res) => {
+  const { name, age } = req.params;
+  return res.json({ "message": `Seu nome é ${name} e você tem ${age} anos de idade` })
+});
+
+app.listen(3001, () => {
+  console.log('Aplicacao ouvindo na porta 3001');
+});
