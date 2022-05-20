7c198ede4dba9460cf9815e7a7a3827499e6d5d3
Carolina Mascarenhas <carolina.mascarenhass96@hotmail.com>
2022-05-20 14:19:27 -0300

    buscando todos os livros do DB

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/model-example/index.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/model-example/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const Author = require('./models/Author');
+const Book = require('./models/Book');
 
 const app = express();
 
@@ -10,7 +11,22 @@ app.get('/authors', async (_req, res) => {
 	res.status(200).json(authors);
 });
 
-const PORT = process.env.PORT || 3000;
+app.get('/books', async (req, res, next) => {
+	const books = await Book.getAll();
+
+	res.status(200).json(books);
+})
+
+app.get('/books/:author_id', async (req, res, next) => {
+	const { author_id } = req.params;
+	const books = await Book.getAll();
+
+	const byAuthorId = books.filter((obj) => obj.author_id === parseInt(author_id));
+
+	res.status(200).json(byAuthorId);
+})
+
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
 	console.log(`Ouvindo a porta ${PORT}`);
