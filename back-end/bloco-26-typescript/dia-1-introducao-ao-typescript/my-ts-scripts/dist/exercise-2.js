49cf121d349262af3f12f01f2d0ae76fb6c66d4d
Carolina Mascarenhas <carolina.mascarenhass96@hotmail.com>
2022-07-06 00:27:08 -0300

    feat: exs 1 a 3 do dia

---
+++ back-end/bloco-26-typescript/dia-1-introducao-ao-typescript/my-ts-scripts/dist/exercise-2.js
@@ -0,0 +1,25 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Seasons = exports.Months = void 0;
+var Months;
+(function (Months) {
+    Months["JAN"] = "Janeiro";
+    Months["FEV"] = "Fevereiro";
+    Months["MAR"] = "Mar\u00E7o";
+    Months["ABR"] = "Abril";
+    Months["MAI"] = "Maio";
+    Months["JUN"] = "Junho";
+    Months["JUL"] = "Julho";
+    Months["AGO"] = "Agosto";
+    Months["SET"] = "Setembro";
+    Months["OUT"] = "Outubro";
+    Months["NOV"] = "Novembro";
+    Months["DEZ"] = "Dezembro";
+})(Months = exports.Months || (exports.Months = {}));
+var Seasons;
+(function (Seasons) {
+    Seasons["PRIMAVERA"] = "Primavera";
+    Seasons["VERAO"] = "Ver\u00E3o";
+    Seasons["OUTONO"] = "Outono";
+    Seasons["INVERNO"] = "Inverno";
+})(Seasons = exports.Seasons || (exports.Seasons = {}));
---
+++ back-end/bloco-26-typescript/dia-1-introducao-ao-typescript/my-ts-scripts/exercise-2.ts
@@ -0,0 +1,21 @@
+export enum Months {
+  JAN = 'Janeiro',
+  FEV = 'Fevereiro',
+  MAR = 'Março',
+  ABR = 'Abril',
+  MAI = 'Maio',
+  JUN = 'Junho',
+  JUL = 'Julho',
+  AGO = 'Agosto',
+  SET = 'Setembro',
+  OUT = 'Outubro',
+  NOV = 'Novembro',
+  DEZ = 'Dezembro'
+}
+
+export enum Seasons {
+  PRIMAVERA = 'Primavera',
+  VERAO = 'Verão',
+  OUTONO = 'Outono',
+  INVERNO = 'Inverno'
+}
